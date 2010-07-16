ac8ea19eb39e4d42aeee98ee4fceacfe7479be5a
Erk Dungan <erik@bigfolio.com>
2010-07-16 11:44:28 -0700

    layout and css for the login and sessions

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,6 +2,8 @@ class UserSessionsController < ApplicationController
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => :destroy
   
+  layout 'sessions'
+  
   def new
     @user_session = UserSession.new
   end
